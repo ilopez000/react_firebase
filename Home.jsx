b4b6ff14167382d8929680d648db313ca5c187d7
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-01-16 18:20:51 +0100

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Bienvenido a Nuestra Aplicación</h1>
+            <p>Esta es la página de inicio de nuestra fantástica aplicación.</p>
+            {/* Aquí puedes agregar más contenido, imágenes, enlaces, etc. */}
+        </div>
+    );
+}
+
+export default Home;
